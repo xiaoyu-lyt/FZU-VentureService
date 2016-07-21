b6fa5a663d8009ebe9554d69ca66eba4a3c390e2
coloryam <machobaby9@hotmail.com>
2016-07-21 23:21:12 +0800

    add usre_student & apply_project

---
+++ Home/venture_web/js/signup_submit.js
@@ -0,0 +1,1 @@
+var submitbtn = document.querySelector('signup-btn');
--- Home/venture_web/js/tabswift.js
+++ Home/venture_web/js/tabswift.js
@@ -23,6 +23,7 @@ function removeClass(elem, className) {
 
 var sideNav = document.querySelector('.side-nav');
 var signupList = document.querySelector('.signup-role-list');
+var userList = document.querySelector('.user-sidenav');
 if(sideNav) {
 	var oli = sideNav.querySelectorAll('li');
 	var box = document.querySelectorAll('.information-detail');
@@ -32,11 +33,16 @@ else if(signupList) {
 	var box = document.querySelectorAll('.signup-table');
 }
 
+else if(userList) {
+	var oli = userList.querySelectorAll('li');
+	var box = document.querySelectorAll('.user-box');
+}
+
 oli = [].slice.call(oli);
 box = [].slice.call(box);
 
 var isClass = hasClass(box[0], 'information-detail');
-
+var isSignup = hasClass(box[0], 'signup-table');
 
 (function switchTab(){
 	oli.forEach(function(elem, index) {
@@ -58,10 +64,13 @@ var isClass = hasClass(box[0], 'information-detail');
 		if(isClass) {
 			box[i].style.display = 'block';
 			addClass(oli[i], 'active');
-		} else {
+			return;
+		} else if(isSignup) {
 			box[i].style.display = 'table';
-			addClass(oli[i], 'now');
+		} else {
+			box[i].style.display = 'block';
 		}
+		addClass(oli[i], 'now');
 	}
 	
 })();
