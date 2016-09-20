a4b6cd53c34ca75dd69556a6c65218cb37f79155
Wibinn <iweibin@sina.cn>
2016-09-20 22:16:52 +0800

    Update config.php

--- API/apps/Home/Conf/config.php
+++ API/apps/Home/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '123.207.58.12', // 服务器地址
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'jyzd',      // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'root',      // 密码
