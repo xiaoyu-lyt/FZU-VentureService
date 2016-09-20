def3f05d6339b2e4f2f29eebd9bd895073de15a9
Wibinn <iweibin@sina.cn>
2016-09-20 22:14:22 +0800

    Update config.php

--- Admin/apps/Home/Conf/config.php
+++ Admin/apps/Home/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 	//'配置项'=>'配置值'
     'DB_PARAMS'             =>   array(\PDO::ATTR_CASE => \PDO::CASE_NATURAL),
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '123.207.58.12', // 服务器地址
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
     'DB_NAME'               =>  'jyzd',      // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  'root',      // 密码
@@ -12,7 +12,7 @@ return array(
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
     'DEFAULT_CONTROLLER'    =>  'Home', // 默认控制器名称
     'DEFAULT_ACTION'        =>  'index', // 默认操作名称
-    'URL_MODEL'             =>  3,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    'URL_MODEL'             =>  1,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
     // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
     'URL_PARAMS_BIND_TYPE'  =>  1, // URL变量绑定的类型 0 按变量名绑定 1 按变量顺序绑定
 
